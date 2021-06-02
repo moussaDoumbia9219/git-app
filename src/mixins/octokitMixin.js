87b9dff306d8e6681221c6fa1010bc45af117dda
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-06-02 00:18:24 +0000

    little refactor

--- src/mixins/octokitMixin.js
+++ src/mixins/octokitMixin.js
@@ -1,11 +1,9 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
-
 export const octokitMixin = {
   methods: {
     createOctokitClient() {
-      return new Octokit({
+      return new window.Octokit({
         auth: localStorage.getItem("github-token"),
       });
-    },
-  },
-};
+    }
+  }
+}
